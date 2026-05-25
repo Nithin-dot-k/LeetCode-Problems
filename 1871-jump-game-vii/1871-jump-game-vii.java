334ed4fe61ae91d14cd9e5ab2a93a78c9235a885
Nithin-dot-k <nithinjr76@gmail.com>
2026-05-25 11:15:23 +0530

    Time: 13 ms (38.37%), Space: 48 MB (42.1%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.java
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -3,19 +3,17 @@ class Solution {
         int n=s.length();
         boolean []res=new boolean[n];
         res[0]=true;
-        List<Integer> l=new ArrayList<>();
-        l.add(0+minJump);
+        int last=0+minJump;
         for(int i=0;i<n-1;i++){
             if(s.charAt(i)=='0' && res[i]){
                 int min=i+minJump;
                 int max=i+maxJump;
                 if(min<n && s.charAt(min)=='0') res[min]=true;
                 if(max<n && s.charAt(max)=='0') res[max]=true;
-                for(int j=Math.max(min,l.get(l.size()-1));j<max;j++){
+                for(int j=Math.max(min,last);j<max;j++){
                     if(j<n && s.charAt(j)=='0') res[j]=true;
-                    l.add(j);
+                    last=j;
                 }
-                System.out.println(min+" "+max);
             }
         }
         return res[n-1];
