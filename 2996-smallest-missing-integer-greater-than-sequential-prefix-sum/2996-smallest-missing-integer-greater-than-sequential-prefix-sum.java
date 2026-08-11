e57d4d362a62b97ad564ecaccfb7d159249e8cf2
Nithin K <nithinjr76@gmail.com>
2026-08-11 11:48:08 +0530

    Time: 1 ms (93.72%), Space: 43.8 MB (88.14%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int missingInteger(int[] nums) {
+        List<Integer> li=new ArrayList<>();
+        for(int i:nums){
+            li.add(i);
+        }
+        boolean flag = true;
+        int sum = 0, nithin = 0;
+       for(int i=1;i<nums.length;i++)
+       {
+            if(nums[i-1]+1 == nums[i])
+            {
+                sum += nums[i-1];
+            }
+            else
+            {
+                sum += nums[i-1];
+                flag = false;
+                break;
+            }
+       }
+       if(flag) sum += nums[nums.length-1];
+       while(nithin < 1)
+       {
+        if(!li.contains(sum))
+        {
+            break;
+        }
+        else
+        {
+            sum += 1;
+        }
+       }
+       return sum;
+    }
+}
